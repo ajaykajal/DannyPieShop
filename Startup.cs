52213345a344cf3a3873914942cbe566e1bb769e
Ajay Kumar <ajay.bf.kumar@accenture.com>
2020-03-04 14:54:05 +0530

    first commit just for message hello message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace DannyPieShop
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World from AJ Kajal!");
                 });
             });
         }
