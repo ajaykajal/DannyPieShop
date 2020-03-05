af15ff3776f82c2183667f6b4a3c3fd230ade1e4
Ajay Kumar <ajay.bf.kumar@accenture.com>
2020-03-05 16:28:34 +0530

    Implement Routing and Menu Navigation

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -29,5 +29,18 @@ namespace DannyPieShop.Controllers
             };
             return View(piesListviewModels);
         }
+
+        /// <summary>
+        /// get particular pie cakes using if
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
---
+++ Migrations/20200305101822_RoutingData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DannyPieShop.Migrations
+{
+    public partial class RoutingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
