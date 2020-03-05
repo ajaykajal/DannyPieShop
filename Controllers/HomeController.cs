9a78196dd80bd94dddfb46e88850cd7681ccd522
ajay <aks7879@gmail.com>
2020-03-05 21:29:57 +0530

    implement home page and partial page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DannyPieShop.Models;
+using DannyPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DannyPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieReository pieReository;
+
+        public HomeController(IPieReository pieReository)
+        {
+            this.pieReository = pieReository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek=pieReository.PieOfTheWeek
+            };
+            return View(homeViewModel);
         }
     }
 }
